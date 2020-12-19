ea461ff34bbeb85077b8c22da31d4044bd0e3780
rico hernandez <rbradt1@saddleback.edu>
2020-12-19 07:16:48 -0800

    include react libraries

--- UI Controllers/Nav.js
+++ UI Controllers/Nav.js
@@ -1,7 +1,9 @@
 function NavModel () {
 	var _this = this;
+	
 	this.currentScene = "home";
 	this.previousScene = "home";
+	
 	this.observers = [];
 	
 	this.setCurrentScene = function(scene) {
@@ -28,6 +30,7 @@ function NavController(model) {
 	var _this = this;
 	this.model = model;
 	
+	
 	this.handleEvent = function(event) {
 		event.stopPropagation();
 		
@@ -49,6 +52,7 @@ function NavView(controller) {
 	document.getElementById("nav_about").addEventListener("click", controller);
 	document.getElementById("nav_support").addEventListener("click", controller);
 	document.getElementById("nav_login").addEventListener("click", controller);
+	
 	this.update = function(model) {
 		console.log(model.getPreviousScene());
 		document.getElementById(model.getPreviousScene()).style.display = "none";
@@ -56,46 +60,3 @@ function NavView(controller) {
 	};
 	this.controller.model.addObserver(this);
 }
-
-//function NavMenuUI() {
-//	//inheritance
-//	Listener.apply(this, arguments);
-//	
-//	//scene
-//	this.current_scene = document.getElementById('home');
-//	
-//	//home button
-//	this.hbIconId = 0;
-//	this.hbIcons = ["angry.png", "yee.png", "annoyed.png", "angry.png", "mad.png", "sad.png"];
-//}
-//
-//NavMenuUI.prototype = Object.create(Listener.prototype);
-//
-//NavMenuUI.prototype.constructor = NavMenuUI;
-//
-//NavMenuUI.prototype.updateListener = function(message, data) {
-//	var button = document.getElementById(message);
-//	var scene_id = message.substr(4);
-//	
-//	switch(String(scene_id)) {
-//		case "home":
-//			var chance = 1;
-//			
-//			if(this.hbIconId != 0 || Math.random() <= chance) {
-//				this.hbIconId = (this.hbIconId + 1)%this.hbIcons.length;
-//				button.style.backgroundImage = "url(" + this.hbIcons[this.hbIconId] + ")";
-//			}
-//			break;
-//				
-//		case "ttt":
-//		case "chess":
-//		case "coming":
-//		case "about":
-//		case "support":
-//		case "login":
-//			this.current_scene.style.display = "none";
-//			this.current_scene = document.getElementById(scene_id);
-//			this.current_scene.style.display = "block"; 
-//			break;
-//	}
-//}
