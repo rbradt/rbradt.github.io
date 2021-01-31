2dcb78b95326487618445f8fa1389603a3160fe3
rbradt <rbradt009@gmail.com>
2021-01-31 13:44:50 -0800

    chess rule logic

--- src/Game/Chess/piece.js
+++ src/Game/Chess/piece.js
@@ -3,6 +3,33 @@ class Move {
         this.row = row;
         this.col = col;
     }
+
+    row() {
+        return this.row;
+    }
+
+    col() {
+        return this.col;
+    }
+
+    exists() {
+        return row < 8 && row > -1 && col < 8 && col > -1;
+    }
+}
+
+class direction {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    delta_row() {return this.y;}
+    delta_col() {return this.x;}
+}
+
+class Directions { 
+    static straight() {return [new direction(0,1), new direction(1,0), new direction(-1,0), new direction(0,-1)]}
+    static diagonal() {return [new direction(1,1), new direction(-1,-1), new direction(-1,1), new direction(1,-1)]}
+    static all() {return [new direction(0,1), new direction(1,0), new direction(-1,0), new direction(0,-1), new direction(1,1), new direction(-1,-1), new direction(-1,1), new direction(1,-1)]}
 }
 
 class Piece {
@@ -35,23 +62,39 @@ class Pawn extends Piece {
 
     }
 
-    performMove(move) {
+    performMove(move, board) {
+        let new_r = move.row();
+        let new_c = move.col();
+        let r_direction = this.isWhitePiece? -1: 1;
 
     }
 
-    possibleMoves(currentBoard) {
+    possibleMoves(board) {
         let moves = [];
         let r = this.row;
         let c = this.col;
+        let r_direction = this.isWhitePiece? -1: 1;
+        let move;
 
+        r += r_direction;
+        move = new Move(r, c);
+        if(board.isEmpty(move))
+            moves.push(move);
+        move = new Move(r, c-1);
+        if(!board.isEmpty(move) && board.pieceAt(move).getSide() != this.side)
+            moves.push(move);
+        move = new Move(r, c+1);
+        if(!board.isEmpty(move) && board.pieceAt(move).getSide() != this.side)
+            moves.push(move);
         
         if(this.isWhitePiece()) {
             if(currentBoard[r-1][c] == null)
                 moves.push(new Move(r - 1, c));
             if(r == 6 && currentBoard[r-2][c] == null)
                 moves.push(new Move(r - 2, c));
-            if()
-        }
+            if(currentBoard[r-1][c-1]!=null && currentBoard[r-1][c-1].getSide != this.side)
+            if(currentBoard[r-1][c+1]!=null && currentBoard[r-1][c+1].getSide != this.side)
+        }r-1 c+1
         else {
 
         }
